24420e17348b384066a7eb6d54aa4ec11490fcfe
Aaron Lai <aaronwk.lai@gmail.com>
2023-03-31 00:45:47 +0800

    Portfolio

---
+++ src/data/BlogData.js
@@ -0,0 +1,20 @@
+// blog data
+
+export const Blogs = [
+  {
+    id: 1,
+    name: "React中的 Array與Key",
+    tags: ["react", "Array", "Key", "Id & index"],
+    date: "24 Mar, 2023",
+    imgSrc: "https://miro.medium.com/v2/resize:fit:1100/0*HvvKYVAp4JFcBicR",
+    link: "https://medium.com/@cd5711438/react%E4%B8%AD%E7%9A%84-array%E8%88%87key-e5643f403d80",
+  },
+  {
+    id: 2,
+    name: "React Scroll Side Bar 呈現",
+    tags: ["react", "styled-components", "useEffect", "useRef"],
+    date: "25 Mar, 2023",
+    imgSrc: "https://miro.medium.com/v2/resize:fit:1100/0*nV-6yH0tIovivFXA",
+    link: "https://medium.com/@cd5711438/react-scroll-side-bar-%E5%91%88%E7%8F%BE-c54d8bf937be",
+  },
+];
---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,106 @@
+import { motion } from "framer-motion";
+import React from "react";
+// import { NavLink } from 'react-router-dom'
+import styled from "styled-components";
+import { Github, Linkedin, Medium } from "../components/AllSvgs";
+import { DarkTheme } from "../components/Themes";
+
+const Icons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: -2.5rem;
+  left: 2rem;
+
+  z-index: 3;
+
+  & > *:not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
+
+const Line = styled(motion.span)`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
+
+const SocialIcons = (props) => {
+  return (
+    <Icons>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={"https://github.com/AaronLai0401"}
+        >
+          <Github
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.2 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={"https://medium.com/@cd5711438"}
+        >
+          <Medium
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}
+      >
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          href={
+            "https://www.linkedin.com/in/%E4%B9%8B%E9%80%A2-%E8%B3%B4-102455223/"
+          }
+        >
+          <Linkedin
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
+        </a>
+      </motion.div>
+
+      <Line
+        color={props.theme}
+        initial={{
+          height: 0,
+        }}
+        animate={{
+          height: "8rem",
+        }}
+        transition={{
+          type: "spring",
+          duration: 1,
+          delay: 0.8,
+        }}
+      />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
