4c890cf7e4d294bd85f5a2af0d7869b17d821637
Aaron Lai <aaronwk.lai@gmail.com>
2023-03-31 11:05:29 +0800

    portfolio更新

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,63 +1,54 @@
 // work data
 
 export const Work = [
-    {
-        id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
-        
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
-    },
-    {
-        id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
-        
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
-        
-    },
-    {
-        id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
-        
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
-    },
-    {
-        id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
-       
-    },{
-        id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    
-  
-] 
+  {
+    id: 1,
+    name: "19 Bistro Order",
+    description:
+      "A system for customers to order food/drink through an in-store digital platform, enhancing the dining experience.",
+    tags: ["React", "Node.js", "Socket io", "MySQL", "StyledComponents"],
+
+    demo: "https://github.com/AaronLai0401/19Bistro",
+    github: "https://github.com/AaronLai0401/19Bistro",
+  },
+  {
+    id: 2,
+    name: "Real-time Weather App for Taiwan",
+    description:
+      "An App for providing up-to-date weather information and forecasts for locations across Taiwan",
+    tags: ["React", "Restful API", "useEffect", "useState"],
+
+    demo: "https://aaronlai0401.github.io/Weather_App/",
+    github: "https://github.com/AaronLai0401/Weather_App",
+  },
+  {
+    id: 3,
+    name: "Booking .clone",
+    description:
+      "A digital platform that allows users to reserve accommodations and admins to manage the system.",
+    tags: ["React", "Node.js", "MongoDB", "Express", "axios"],
+
+    demo: "https://github.com/AaronLai0401/MERN_Booking",
+    github: "https://github.com/AaronLai0401/MERN_Booking",
+  },
+  {
+    id: 4,
+    name: "NFT Website",
+    description:
+      "A responsive design approach for static webpages to ensure optimal viewing experience on different devices.",
+    tags: ["Html", "Sass", "JavaScript"],
+
+    demo: "https://aaronlai0401.github.io/NFT-website/",
+    github: "https://github.com/AaronLai0401/NFT-website",
+  },
+  {
+    id: 5,
+    name: "2D Anya Breakout",
+    description:
+      "A game based on the popular manga series 'Spy Family', featuring challenging levels",
+    tags: ["Html", "Css", "JavaScript"],
+
+    demo: "https://github.com/AaronLai0401/2D_Anya_BreakOut",
+    github: "https://github.com/codebucks27/react-sidebar",
+  },
+];
